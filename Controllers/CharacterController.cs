9c03dcb0c3271a7aa3cd520c5d9807854a849262
Aither <cheese736@hotmail.com>
2023-05-05 17:47:11 +0800

    add CharacterService

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,26 +10,27 @@ namespace dotnet_rpg.Controllers
   [Route("api/[controller]")]
   public class CharacterController : ControllerBase
   {
-    private static List<Character> characters = new List<Character>{
-      new Character(),
-      new Character{ Id = 1,Name = "Sam" }
-    };
+    private readonly ICharacterService _characterService;
+
+    public CharacterController(ICharacterService characterService )
+    {
+      _characterService = characterService;
+    }
 
     [HttpGet("GetAll")]
-    public ActionResult<List<Character>> Get()
+    public async Task<ActionResult<List<Character>>> Get()
     {
-      return Ok(characters);
+      return Ok(await _characterService.GetAllCharacters());
     }
     [HttpGet("{id}")]
-    public ActionResult<List<Character>> GetSingle(int id)
+    public async Task<ActionResult<List<Character>>> GetSingle(int id)
     {
-      return Ok(characters.FirstOrDefault(c => c.Id == id));
+      return Ok(await _characterService.GetCharacterById(id));
     }
     [HttpPost]
-    public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+    public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
     {
-      characters.Add(newCharacter);
-      return Ok(characters);
+      return Ok(await _characterService.AddCharacter(newCharacter));
     }
   }
 }
