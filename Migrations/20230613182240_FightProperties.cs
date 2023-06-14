610f696a5736015a59408e90b3ad3d329719df50
Aither <cheese736@hotmail.com>
2023-06-14 22:36:31 +0800

    add FightProperties and implement logic of battle

---
+++ Migrations/20230613182240_FightProperties.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    /// <inheritdoc />
+    public partial class FightProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skills_Users_UserId",
+                table: "Skills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Skills_UserId",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Skills");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Defeats",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fights",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Victories",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Defeats",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Fights",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Victories",
+                table: "Characters");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Skills",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_UserId",
+                table: "Skills",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skills_Users_UserId",
+                table: "Skills",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
